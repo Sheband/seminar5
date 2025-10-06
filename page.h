b6bbfd680b86c6e0303abcdf459e8669e0a45a30
vladimir <vladimir.oleynyk057@gmail.com>
2025-10-06 12:31:41 +0300

    final version

---
+++ page.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+std::string generateHtmlHeader(const std::string &title);
+
+void generateCssGradient(const std::string &color1, const std::string &color2);
+void createSimpleHtmlPage();
+
+void generateMultipleGradients();
